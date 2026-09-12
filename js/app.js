12c2334e869e57985bb0dfae958016e852db9db8
kohei0320 <ycsvstskwn@privaterelay.appleid.com>
2026-09-12 11:13:19 +0900

    app.js を更新

--- js/app.js
+++ js/app.js
@@ -277,9 +277,6 @@ const App = (() => {
   function isCustomQuestion(question) {
     return !!(question && question.custom === true);
   }
-function isCustomQuestion(question) {
-  return !!(question && question.custom === true);
-}
 
 function getCustomQuestions() {
   return questions.filter(question => isCustomQuestion(question));
